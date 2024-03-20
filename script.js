9546a2c4e5794dba9fa71c1c5b508d3f94b79f8a
Nadine Grantz <nadine.grantz@googlemail.com>
2024-03-20 13:12:25 +0100

    add js file

---
+++ script.js
@@ -0,0 +1,20 @@
+let dictionary = {
+  Auto: "Car",
+  Haus: "House",
+  Stuhl: "Chair",
+  Tisch: "Table",
+  Buch: "Book",
+  Fenster: "Window",
+  Hund: "Dog",
+  Katze: "Cat",
+};
+
+function addVocabulary() {
+  let deutschText = document.getElementById("deutschText");
+  let englischText = document.getElementById("englischText");
+
+  if (deutschText.value && englischText.value) {
+    dictionary[deutschText.value] = englischText.value;
+    console.log(dictionary);
+  }
+}
