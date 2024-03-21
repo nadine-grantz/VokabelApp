e4991c8fa23c14b72cafc1a09408872c6b857b13
Nadine Grantz <nadine.grantz@googlemail.com>
2024-03-21 08:52:36 +0100

    finish localstorage

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-let dictionary = {
-  Auto: "Car",
-  Haus: "House",
-  Stuhl: "Chair",
-  Tisch: "Table",
-  Buch: "Book",
-  Fenster: "Window",
-  Hund: "Dog",
-  Katze: "Cat",
-};
+const dictionary = JSON.parse(localStorage.getItem("dictionary")) || {};
 
 function addVocabulary() {
-  let deutschText = document.getElementById("deutschText");
-  let englischText = document.getElementById("englischText");
+  const deutschText = document.getElementById("deutschText");
+  const englischText = document.getElementById("englischText");
 
   if (deutschText.value && englischText.value) {
     dictionary[deutschText.value] = englischText.value;
-
+    console.log("das ist der wert:", deutschText.value);
     deutschText.value = "";
     englischText.value = "";
   }
+  localStorage.setItem("dictionary", JSON.stringify(dictionary));
+  console.log(dictionary);
 }
